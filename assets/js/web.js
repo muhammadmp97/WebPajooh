1a27fe9515fdd668b43a3b9a89cd7add0f4802e2
webpajooh <mohammadmp7676@gmail.com>
2018-11-14 16:39:09 +0330

    New: animated cText with animate.css

--- assets/js/web.js
+++ assets/js/web.js
@@ -1,8 +1,8 @@
 const appURL = location.href.replace(location.hash,"");
 let pbVisited = false;
 let portfolio = {}, repositories = {};
-$.getJSON(appURL+"assets/portfolio.json", function(data){portfolio=data;});
-$.getJSON("https://api.github.com/users/webpajooh/repos?sort=updated&direction=asc", function(data){repositories=data;});
+//$.getJSON(appURL+"assets/portfolio.json", function(data){portfolio=data;});
+//$.getJSON("https://api.github.com/users/webpajooh/repos?sort=updated&direction=asc", function(data){repositories=data;});
 
 //-> General Functions
 function portfolioRender(count = portfolio.length){
@@ -61,7 +61,6 @@ $(document).ajaxComplete(function(){
     portfolioRender(4);
     repositoriesRender(3);
 });
-
 
 $(document).ready(function(){
     let ctHeight = $('#coverText').outerHeight() + 250;
